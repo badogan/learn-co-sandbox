c70a07fa59bcbc274799b754d6729d66d271bedd
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-14 20:12:12 +0000

    Done.

--- basri-tests-12Nov19-1125.rb
+++ basri-tests-12Nov19-1125.rb
@@ -36,20 +36,60 @@ fourth_challenge[:capulet].map {|hash_key,hash_value|
    }
  end
 }
+
 p total
 
 #Basri - simple array tests
+def BasriAddAnElementToAnArray(hash,key,new_element)
+#Objective: Function appends a key, value pair with a new element
+#Input: hash, key, element
+#       value associated with the key is an array
+#Output: updated hash
+hash[key][hash[key].length] = new_element
+hash
+end
+
+def BasriAddANewTownWithInitialKeysValues(current_full_hash,new_town_name)
+#Objective: Function appends a new set of keys,values for the new town 
+#Input:     current_full_hash is the hash to be updated with the new town key,value pairs
+#           new_town_name
+#Output:    updated current_full_hash
+current_full_hash[new_town_name] = {
+  :population=>nil,
+  :important_people=>[]
+}
+current_full_hash #Now updated!
+end
+
+def BasriAddANewRootKeyValuePair(input_hash,new_initial_key,new_initial_value)
+#Objective:   Function adds a new key,value pair to the top level on the provided hash
+#Input:       current_full_hash is the hash to be updated with new key,value pair
+#             new_key is the new key
+#             new_value is the new value
+#Output:      full_hash_updated with the new key,value pair
+input_hash[new_initial_key]=new_initial_value
+input_hash
+end
+
 def basri_function
 towns_details = {}
-towns_details = {:twyford=>{},
-  :sonning=>{},
-  :charvil=>{}
-}
-towns_details[:twyford] = {:population=>nil,:important_people=>nil}
-towns_details[:charvil] = {:population=>nil,:important_people=>nil}
-towns_details[:sonning] = {:population=>nil,:important_people=>nil}
+towns_details = BasriAddANewTownWithInitialKeysValues(towns_details,:twyford)
+towns_details = BasriAddANewTownWithInitialKeysValues(towns_details,:sonning)
+towns_details = BasriAddANewTownWithInitialKeysValues(towns_details,:charvil)
 towns_details[:twyford][:population] = 5000
+towns_details[:sonning][:important_people] = ["Clooneys"]
+towns_details[:twyford] = BasriAddAnElementToAnArray(towns_details[:twyford],:important_people,"The Dogans")
+towns_details[:twyford] = BasriAddAnElementToAnArray(towns_details[:twyford],:important_people,"Lisa & Garry and Co")
+towns_details[:twyford] = BasriAddANewRootKeyValuePair(towns_details[:twyford],:team_colours,[])
+towns_details[:twyford] = BasriAddAnElementToAnArray(towns_details[:twyford],:team_colours,"red")
+towns_details[:charvil] = BasriAddANewRootKeyValuePair(towns_details[:charvil],:team_colours,["purple","blue"])
+#binding.pry
 towns_details
 end
 
+#require 'pry'
+
 p basri_function
+#=======
+p total
+
